df755c13b9401a01e572fbef7ef2870e94d1ea8e
Min Cheung <ilove52345234@gmail.com>
2020-01-07 12:22:56 +0800

    add read file

--- products.py
+++ products.py
@@ -1,8 +1,15 @@
 #Produts 建立記帳小程式
 #二維清單
-
-
+#讀取檔案
 products = []
+with open('products.csv', 'r', encoding = 'utf-8') as f:
+	for line in f:
+		if '商品, 價格' in line:
+			continue ##從這跳到下個迴圈 這迴圈後面不執行
+		name, price = line.strip().split(',')
+		products.append([name, price])
+	print(products)
+##讓使用者輸入
 while True:
     name = input('請輸入商品名稱: ' )
     if name == 'q':
@@ -17,14 +24,16 @@ while True:
 print(products)
 #products[0][0]   
 #索引 第0個清單內的清單第0個選項
+
+#印出所有購買紀錄
 for p in products:
 	print(p[0], '的價格是', p[1])
-
 #再次提醒 with 為自動關閉功能
 #字串可以用加法跟乘法 
 #減法跟除法不行
 #.csv 常用儲存資料檔案 可用excel開啟 ,為分隔
 
+##寫入檔案
 with open('products.csv', 'w', encoding = 'utf-8') as f:
 	f.write('商品, 價格\n')
 	for p in products:
